8d08fa160656f73b7ac781f6956ef868e296a921
Mojo <jomoreautrb@gmail.com>
2021-12-16 12:40:17 -0500

    16 décembre caroussel

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Header from './components/header';
 import './App.css';
 import Menu from './components/nav.js';
-
+import Presentation from './components/presentation.js';
+import Bio from './components/bio.js';
 
 function App() {
   return (
     <div >
       <Header />
       <Menu />
+      <Presentation />
+      <Bio />
     </div>
   );
 }
---
+++ src/components/presentation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../index.css';
+
+
+
+
+
+const Presentation = ({ presentation }) => {
+  return (
+    <div className='div_mission'>
+      <h1 className='h1'>Mission de l'ensemble</h1>
+      <p className='p_mission'>Quinette de cuivre, Notre mission est de faire découvrir la musique sous un autre angle en utilisant les winstruments de cuivres afin 
+        d’interpréter la musique déjà connue. Musique éclectique, issue du classique, du jazz, de la pop et même des jeux vidéo, c’est l’approche 
+        que nous avons choisie pour être en mesure de personnaliser vos événements avec des pièces connues et moins connues. </p>
+
+    </div>
+    
+  ); 
+}
+
+Presentation.defaultProps = {
+  presentation: 'Mission',
+}
+
+
+
+
+export default Presentation
